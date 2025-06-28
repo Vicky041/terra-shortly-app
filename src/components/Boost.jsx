cf71cd8b92c7acbe6d507f4ffea97cc2ba81ecd2
Adachukwu24 <110688404+Adachukwu24@users.noreply.github.com>
2025-06-28 07:48:36 +0100

    Adjusted responsiveness to footer (#8)
    Co-authored-by: Collins Adaigho <106975277+Donsmatt@users.noreply.github.com>

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -5,7 +5,7 @@ export default function Boost() {
   return (
     <div class="boost-container">
         <h2>Boost your links today</h2>
-        <Button label={"Get Started"}></Button>
+        <a href=""><Button label={"Get Started"}></Button></a>
     </div>
   )
 }
