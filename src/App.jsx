378c1b4267f2db7dbd683c09208bc8fb286f4eaa
Collins <donsmatt44@gmail.com>
2025-06-28 07:39:54 +0100

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import Hero from "./components/hero";
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
-        URL Shortener
-      </h1>
       <Hero />
-      {/* <ShortenProvider>
+      <ShortenProvider>
         <CallToAction />
       </ShortenProvider>
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,19 @@
-import React from "react";
 import "../styles/hero.css";
 import Button from "./Button";
-import Boost from "./Boost";
 
 export default function Hero() {
   return (
     <div className="hero-container">
-      <div className="hero-content">
+      <div className="hero-content container">
         <div className="hero-subContainer">
-          <h1>
-            <span className="hero-span">More than just</span>
-            <span className="hero-span">shorter links</span>
-          </h1>
-          <h6>
+          <h1>More than just shorter links</h1>
+          <p>
             Build your brand's recognition and get detailed insights on how your
             links are performing.
-          </h6>
-          <button className="hero-butt">Get Started</button>
+          </p>
+          <div>
+            <Button label="Get started" variant="secondary" />
+          </div>
         </div>
         <div className="hero-section">
           <img
