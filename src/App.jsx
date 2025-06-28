d3607dd8ae74bcba96208d6cfe2cc097c2a6ba27
Collins <donsmatt44@gmail.com>
2025-06-28 22:13:14 +0100

    Fix case sensitivity for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CallToAction from "./ui/CallToAction";
 import ShortenProvider from "./contexts/ShortenContext";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import AdvanceStatistics from "./ui/AdvanceStatistics";
 
 function App() {
